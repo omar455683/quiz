288c59e6b798fe08620ddcefa5c1da3596ecc318
omar455683 <63262606+omar455683@users.noreply.github.com>
2020-04-07 20:52:48 +0300

    Update look.js

--- look.js
+++ look.js
@@ -11,13 +11,47 @@
 
       var app={
                 questions:[
-                          {q:'اخبارك',n:'omar', options:['الحمدلله','بخير','لاباس','زي وجهك',],answer:4},
-						  
-						   {q:'يا رب تزبط ?',n:'noor',options:['TD','br','P','tr',],answer:1},
-						   
-						    {q:'حلو كثير ?',n:'ali',options:['TD','br','P','tr',],answer:1},
 
-                          {q:'Correct HTML tag for the largest heading is ?',n:'ahmad',options:['h4','h1','h8','h9',],answer:2}
+						  
+
+	
+   
+
+
+          {q:'اي من الكلمات التالية تأتي بمعنى - سيارته ?',n:'نستخدم ضمير الملكيه عند الحديث عن مذكر لا نوجه له الكلامhis',options:['his car','her car','our car','my car',],answer:1},
+
+   {q:'ترجم هذه الكلمة - كتابي?',n:'استخدم ضمير الملكية my عندما اتحدث عن شيء أملكه انا',options:['her book','my book','our book','his book',],answer:2},
+
+   {q:'اي من الكلمات التالية تأتي بمعنى - كلبهم ?',n:'استخدم ضمير الملكية their عندما اتحدث عن شيء يملكه اكثر من شحص',options:['our dog','my dog','his dog','their dog',],answer:4},
+
+   {q:'ترجم هذه الكلمة - she took my pen?',n:'استخدم ضمير الملكية my عندما اتحدث عن شيء أملكه انا  ',options:['هي أخذت كتابها','هي أخذت كتابي','هي أخذت كتابهم','هي أخذت كتابه',],answer:2},
+
+   {q:'ترجم هذه الجملة-انا اكلت شطيرته ?',n:'نستخدم ضمير الملكيه عند الحديث عن مذكر لا نوجه له الكلام his',options:['I ate his sandwich','I ate her sandwich','I ate my sandwich','I ate our sandwich',],answer:1},
+
+   {q:'ترجم جمله - معلمنا?',n:'يستخدم ضمير الملكيه our عندما نتحدث عن شيء نملكه نحن',options:['my teacher','his teacher','their teacher','our teacher',],answer:4},
+
+   {q:'ما معنى كلمة - مدونتي?',n:'نستخدم ضمير الملكية my عندما اتحدث عن شيء أملكه انا',options:['my blog','our blog','her blog','its blog',],answer:1},
+
+   {q:'معنى كلمة - القطة تأكل طعامها?',n:'نستخم ضمير الملكيه its عندما نتحدث عن شيء غير عاقل',options:['the cat eats his food','the cat eats my food','the cat eats its food','the cat eats her food',],answer:3},
+
+   {q:'her school ?',n:'ضمير ملكيه يعود على مؤنث her ',options:['مدرستي','مدرستها','مدرستنا','مدرستهم',],answer:2},
+   
+    {q:'my ring ?',n:'الضمير my يعود على شيء امتلكه انا',options:['خاتمنا','خاتمه','خاتمي','خاتمها',],answer:3},
+
+          {q:'their bag ?',n:'استخدم ضمير الملكية their عندما اتحدث عن شيء يملكه اكثر من شحص',options:['حقيبتهم','حقيبتي','حقيبتها','حقيبتنا',],answer:1},
+
+   {q:'her phone ?',n:'ضمير ملكيه يعود على مؤنث her',options:['هاتفي','هاتفنا','هاتفها','هاتفهم',],answer:3},
+
+   {q:'كرسينا ?',n:'يستخدم ضمير الملكيه our عندما نتحدث عن شيء نملكه نحن',options:['our chair','my chair','her chair','its chair',],answer:1},
+
+   {q:'my friend ?',n:'نستخدم ضمير الملكية my عندما اتحدث عن شيء أملكه انا',options:['صديقنا','صديقها','صديقهم','صديقي',],answer:4},
+
+   {q:'أخاك ?',n:'your يستخدم ضمير للملكة للحديث عن شيء يملكه الشخص الذي تتحدث معه',options:['my brother','your brother','our brother','his brother},answer:2}
+
+					   
+
+
+
                           ],
                 index:0,
                 load:function(){
